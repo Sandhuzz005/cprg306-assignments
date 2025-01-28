ffe4a5012ebde78f327a9b61e2526cf31eedaf1d
Sandhuzz005 <kaurharpreet6119@gmail.com>
2025-01-28 15:06:43 -0700

    commit final

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,6 +4,6 @@ import React from 'react';
 export default function Page(){
     return <main>
         <p>Harpreet Kaur</p>
-        <Link href="https://github.com/Sandhuzz005/cprg306-assignments.git"/>
+        <Link href="https://github.com/Sandhuzz005/cprg306-assignments.git">My github repository</Link>
     </main>
 }
