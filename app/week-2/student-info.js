f8fabf5216f7391279910ffdfc22c8ec70751f61
Sandhuzz005 <kaurharpreet6119@gmail.com>
2025-01-28 14:59:04 -0700

    commit code

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import React from 'react';
+
+export default function Page(){
+    return <main>
+        <p>Harpreet Kaur</p>
+        <Link href="https://github.com/Sandhuzz005/cprg306-assignments.git">My github repository</Link>
+    </main>
+}
